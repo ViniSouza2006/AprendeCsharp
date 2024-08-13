a55557c0c3aa89f131e2a2d0dea8ed8a03f048d9
ViniSouza2006 <146894459+ViniSouza2006@users.noreply.github.com>
2024-08-13 16:37:27 -0300

    UploadArquivos02

---
+++ Cap02_EX03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_EX03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float HT, VH, PD, SB, TD, SL;
+            /*HT = HORA TRABALHADA   VH = VALOR DA HORA   PD = PERCENTUAL DE DESCONTO
+              SB =                   TD =                 SL = SALARIO LIQUIDO       */
+
+            Console.WriteLine("Horas trabalhadas ............: ");//WRITELINE = Escreva na linha 
+            HT = float.Parse(Console.ReadLine());// FLOAT = Numeros flutuantes (". ,")
+            Console.WriteLine("Valor da hora ............: ");
+            VH = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor do percentual de desconto .: ");
+            PD = float.Parse(Console.ReadLine());
+
+            SB = HT * VH;//SB é atribuido ao valor de HT * VH
+            TD = (PD / 100) * SB;//TD é atribuido ao valor de PD / 100
+            SL = SB - TD;//SL é atribuido ao valor de SB - TD
+
+
+            Console.WriteLine();
+            Console.WriteLine("Salario liquido ............: ");
+            Console.WriteLine(SL.ToString("##,##0.00"));
+
+            Console.WriteLine();
+            Console.WriteLine("Tecla <Enter> para encerrar... ");
+            Console.ReadLine();//Aguarda a finalização do programa 
+        }
+    }
+}
---
+++ Cap02_Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string NOME;
+
+            Console.Write("Entre seu nome");//Leia teclado, Write(Escreva) Exibe o valor guardado
+            NOME = Console.ReadLine();//Lê a proxima linha
+            Console.Write("Ola, " + NOME);//Guarda o valor das string então exibe o valor guardado em nome   
+            Console.WriteLine("");//Pula-linha
+            Console.WriteLine("Tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+        }
+    }
+}
